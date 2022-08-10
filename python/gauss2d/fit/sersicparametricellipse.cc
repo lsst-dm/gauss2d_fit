85adbafb1028ad9cec4c4006658ddcfc88c9de73
taranu <dan.s.taranu@gmail.com>
2022-08-09 21:19:56 -0400

    Fix SersicMixComponent

--- python/gauss2d/fit/sersicparametricellipse.cc
+++ python/gauss2d/fit/sersicparametricellipse.cc
@@ -1,7 +1,7 @@
 /*
  * This file is part of gauss2dfit.
  *
- * Developed for the LSST SersicComponent Management System.
+ * Developed for the LSST Data Management System.
  * This product includes software developed by the LSST Project
  * (https://www.lsst.org).
  * See the COPYRIGHT file at the top-level directory of this distribution
