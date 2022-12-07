678a582b6676f6fb1ef700de77f9f8c2416d6df6
taranu <dan.s.taranu@gmail.com>
2022-12-07 17:49:43 -0500

    Move channel registry out of header

--- tests/test_channel.cc
+++ tests/test_channel.cc
@@ -14,8 +14,12 @@ TEST_CASE("Channels")
     CHECK(x->get_channels().size() == 2);
     CHECK_THROWS_AS(g2f::Channel::erase("None"), std::invalid_argument);
     CHECK_THROWS_AS(g2f::Channel::erase("x"), std::runtime_error);
+    CHECK_THROWS_AS(g2f::Channel::make("x"), std::invalid_argument);
     x.reset();
     g2f::Channel::erase("x");
     CHECK(g2f::Channel::get_channels().size() == 1);
-    CHECK( g2f::Channel::make("x")->get_channels().size() == 2);
+    CHECK(g2f::Channel::make("x")->get_channels().size() == 2);
+    CHECK(g2f::Channel::find_channel("y") == nullptr);
+    CHECK(g2f::Channel::get_channel("x")->get_channels().size() == 2);
+    CHECK(g2f::Channel::get_channel("y")->get_channels().size() == 3);
 }
