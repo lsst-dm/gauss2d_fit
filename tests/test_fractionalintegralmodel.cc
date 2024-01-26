1870754f65a14f4b0bf23ee0a5aedb0128c922fd
taranu <dan.s.taranu@gmail.com>
2024-01-26 15:32:10 -0500

    Convert CHECK calls to specific CHECK_s

--- tests/test_fractionalintegralmodel.cc
+++ tests/test_fractionalintegralmodel.cc
@@ -17,31 +17,31 @@ TEST_CASE("FractionalIntegralModel") {
     auto frac = g2f::FractionalIntegralModel::make(std::nullopt, integral);
 
     // Check that the parent is set correctly and gives the right value
-    CHECK(&(frac->get_parent_model()) == integral.get());
-    CHECK(frac->at(channel)->get_value() == 0);
-    CHECK(frac->get_integral_remainder(channel) == value);
+    CHECK_EQ(&(frac->get_parent_model()), integral.get());
+    CHECK_EQ(frac->at(channel)->get_value(), 0);
+    CHECK_EQ(frac->get_integral_remainder(channel), value);
 
     // Set the fraction to 100% (previously 0)
     frac->at(channel)->set_value(1.);
-    CHECK(frac->get_integral(channel) == value);
-    CHECK(frac->get_integral_remainder(channel) == 0);
+    CHECK_EQ(frac->get_integral(channel), value);
+    CHECK_EQ(frac->get_integral_remainder(channel), 0);
 
     // Check that setting to 25% works
     double frac1 = 0.25;
     frac->at(channel)->set_value(frac1);
-    CHECK(frac->at(channel)->get_value() == frac1);
-    CHECK(frac->get_integral(channel) == frac1 * value);
-    CHECK(frac->get_integral_remainder(channel) == (1. - frac1) * value);
+    CHECK_EQ(frac->at(channel)->get_value(), frac1);
+    CHECK_EQ(frac->get_integral(channel), frac1 * value);
+    CHECK_EQ(frac->get_integral_remainder(channel), (1. - frac1) * value);
 
     // Check that the frac gives both parameters (parent's integral and its own fraction)
     g2f::ParamCRefs params{};
-    CHECK(frac->get_parameters_const(params).size() == 2);
+    CHECK_EQ(frac->get_parameters_const(params).size(), 2);
 
     // Add another frac and valifate it
     auto frac2 = g2f::FractionalIntegralModel::make(std::nullopt, frac);
     frac2->at(channel)->set_value(1.);
-    CHECK(frac2->at(channel)->get_value() == 1.);
-    CHECK(frac2->get_integral(channel) == (1 - frac1) * value);
+    CHECK_EQ(frac2->at(channel)->get_value(), 1.);
+    CHECK_EQ(frac2->get_integral(channel), (1 - frac1) * value);
     CHECK(frac->find_model(*integral) == nullptr);
 
     std::shared_ptr<g2f::IntegralModel> integral_base = std::make_shared<g2f::LinearIntegralModel>(nullptr);
--- tests/test_sersicmixcomponent.cc
+++ tests/test_sersicmixcomponent.cc
@@ -14,8 +14,8 @@ namespace g2f = gauss2d::fit;
 TEST_CASE("SersicMixComponentIndexParameter") {
     auto param = g2f::SersicMixComponentIndexParameter(0.5);
     CHECK_THROWS(param.set_limits(std::make_shared<const parameters::Limits<double>>(0.3, 8.1)));
-    CHECK(param.get_limits_maximal().get_min() == 0.5);
-    CHECK(param.get_limits().get_min() == 0.5);
+    CHECK_EQ(param.get_limits_maximal().get_min(), 0.5);
+    CHECK_EQ(param.get_limits().get_min(), 0.5);
     CHECK_THROWS_AS(param.set_value(0.5 - 1e-10), std::runtime_error);
     param.set_limits(std::make_shared<const parameters::Limits<double>>(1.0, 4.0));
     CHECK_THROWS_AS(param.set_value(1.0 - 1e-10), std::runtime_error);
@@ -23,8 +23,11 @@ TEST_CASE("SersicMixComponentIndexParameter") {
 }
 
 TEST_CASE("SersicMixComponent") {
-    auto sersic_n = std::make_shared<g2f::SersicMixComponentIndexParameter>(1.0);
-    CHECK(sersic_n->get_order() == 4);
+    auto sersic_n = std::make_shared<g2f::SersicMixComponentIndexParameter>(2.0);
+    CHECK_EQ(sersic_n->get_order(), 4);
+    CHECK_EQ(sersic_n->get_value(), 2.0);
+    sersic_n->set_value(1.0);
+    CHECK_EQ(sersic_n->get_value(), 1.0);
 
     const auto& C = g2f::Channel::NONE();
 
@@ -39,11 +42,11 @@ TEST_CASE("SersicMixComponent") {
             std::make_shared<g2f::SersicParametricEllipse>(reff_x, reff_y),
             std::make_shared<g2f::CentroidParameters>(), std::make_shared<g2f::LinearIntegralModel>(&data),
             sersic_n);
-    CHECK(comp->str().size() > 0);
+    CHECK_GE(comp->str().size(), 0);
 
-    CHECK(comp->get_n_gaussians(C) == sersic_n->get_order());
+    CHECK_EQ(comp->get_n_gaussians(C), sersic_n->get_order());
     const auto gaussians = comp->get_gaussians(C);
-    CHECK(gaussians->size() == sersic_n->get_order());
+    CHECK_EQ(gaussians->size(), sersic_n->get_order());
 
     double integral = 0;
     double sigma_old = 1e31;
@@ -51,25 +54,27 @@ TEST_CASE("SersicMixComponent") {
         integral += g->get_integral_value();
         const auto& ell = g->get_ellipse_const();
         double sigma = ell.get_sigma_x();
-        CHECK(sigma > 0);
-        CHECK(sigma < sigma_old);
-        CHECK(ell.get_sigma_y() == sigma);
-        CHECK(ell.get_rho() == 0);
+        CHECK_GT(sigma, 0);
+        CHECK_LT(sigma, sigma_old);
+        CHECK_EQ(ell.get_sigma_y(), sigma);
+        CHECK_EQ(ell.get_rho(), 0);
         sigma_old = sigma;
     }
-    CHECK(integral == total);
+    CHECK_EQ(integral, total);
 
     sersic_n->set_value(0.5);
     const auto gauss = comp->get_gaussians(C);
-    CHECK(gauss->at(0).get_integral_value() == total);
-    for (size_t i = 1; i < sersic_n->get_order(); ++i) CHECK(gauss->at(i).get_integral_value() == 0);
+    CHECK_EQ(gauss->at(0).get_integral_value(), total);
+    for (size_t i = 1; i < sersic_n->get_order(); ++i) {
+        CHECK_EQ(gauss->at(i).get_integral_value(), 0);
+    }
 
     // 3 ellipse, 2 centroid, one integral, one Sersic index
     const size_t n_params = 7;
 
     g2f::ParamRefs params;
-    CHECK(comp->get_parameters(params).size() == n_params);
+    CHECK_EQ(comp->get_parameters(params).size(), n_params);
 
     g2f::ParamCRefs cparams;
-    CHECK(comp->get_parameters_const(cparams).size() == n_params);
+    CHECK_EQ(comp->get_parameters_const(cparams).size(), n_params);
 }
